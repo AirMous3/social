ec49243ab7982213ae411535547ecef9b2b77eb4
AirMous3 <airmouselol@gmail.com>
2021-08-12 16:45:06 +0300

    added page Users and UsersReducer (follow, unfollow, set-users)

--- src/redux/ActionsTypes.ts
+++ src/redux/ActionsTypes.ts
@@ -1,8 +1,12 @@
 import {addPostActionCreator, newPostTextActionCreator} from "./profileReducer";
 import {addNewMessageActionCreator, newMessageTextActionCreator} from "./dialogsReducer";
+import {followAC, setUsersAC, unfollowAC} from "./UsersReducer";
 
 export type ActionsType =
     ReturnType<typeof addPostActionCreator>
     | ReturnType<typeof newPostTextActionCreator>
     | ReturnType<typeof addNewMessageActionCreator>
     | ReturnType<typeof newMessageTextActionCreator>
+    | ReturnType<typeof followAC>
+    | ReturnType<typeof unfollowAC>
+    | ReturnType<typeof setUsersAC>
