4dd0d0e5ee525566632d2dd92207fa73727c95c0
AirMous3 <airmouselol@gmail.com>
2021-09-19 20:38:26 +0300

    add loginForm

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 export const Login = () => {
-    return <h1>
-        Login
-    </h1>
+    return (
+        <div>
+            <h1>
+                Login
+            </h1>
+            <LoginForm />
+        </div>
+    )
+}
+
+const LoginForm = () => {
+    return (
+        <form >
+            <div>
+                <input type="text" placeholder={"your login"} />
+            </div>
+            <div>
+                <input type="text" placeholder={"your password"} />
+            </div>
+            <div>
+                <input type="checkBox" /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+
+        </form>
+    )
 }
