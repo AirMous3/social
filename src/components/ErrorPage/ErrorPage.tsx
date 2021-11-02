686da7c9190d3c048cc8ef6b7fb55c2e4d746321
AirMous3 <airmouselol@gmail.com>
2021-11-02 15:59:29 +0300

    delete navLink

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,17 +1,18 @@
 import s from './ErrorPage.module.css'
 import SuperButton from "../common/SuperButton/SuperButton";
-import { NavLink } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 export const Error = () => {
+    let history = useHistory()
+    const onCLickHandler = () => {
+        return history.push('/profile')
+    }
     return (<div className={s.wrapper}>
         <h1>
             Woops, page not found 404
         </h1>
-        <SuperButton>
-
-            <NavLink style={{textDecoration: 'none'}} to={'/profile'}>
-                return
-            </NavLink>
+        <SuperButton onClick={() => onCLickHandler()}>
+            return
         </SuperButton>
 
     </div>)
